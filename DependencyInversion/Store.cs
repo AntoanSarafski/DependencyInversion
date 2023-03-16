761ac6ae87e9fe9a5c4265d2ddf10e4d8e547fbb
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-16 09:41:31 +0200

    Created DI method.

--- DependencyInversion/Store.cs
+++ DependencyInversion/Store.cs
@@ -23,5 +23,23 @@ namespace DependencyInversion
             }
             return "No promotion.";
         }
+
+        public string GetPromotionTodayDI(DateTime dateToday)
+        {
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)   // WE CAN NOT TEST THIS CODE, UNTIL MONDAY :( THIS IS UNACCEPTABLE !!!
+            {
+                return "MondayPromotion";
+            }
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)   // WE CAN NOT TEST THIS CODE, UNTIL FRIDAY :( THIS IS UNACCEPTABLE !!!
+            {
+                return "FridayPromotion";
+            }
+            if (DateTime.Now.Year == 2030)                    // WE CAN NOT TEST THIS CODE, UNTIL 2030 :(( THIS IS UNACCEPTABLE !!!
+            {
+                return "PromoYear";
+            }
+            return "No promotion.";
+        }
     }
 }
