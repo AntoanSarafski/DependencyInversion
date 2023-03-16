a8dd62962dc6bd0a0c487ff57a3dc1d5bc7f1df1
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-16 09:30:12 +0200

    Add project files.

---
+++ DependencyInversion/Store.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInversion
+{
+    public class Store
+    {
+        public string GetPromotionToday()
+        {
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                return "MondayPromotion";
+            }
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                return "FridayPromotion";
+            }
+            return "No promotion.";
+        }
+    }
+}
